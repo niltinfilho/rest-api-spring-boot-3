8e74159def4e3c1b4a94e00b928c94d247124ad5
niltinfilho <niltonfilho115@gmail.com>
2025-10-29 11:19:41 -0300

    Recurso para localizar um usuário pelo id

--- demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/service/UsuarioService.java
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/service/UsuarioService.java
@@ -2,9 +2,9 @@ package br.app.gestaotec.demo_park_api.service;
 
 import br.app.gestaotec.demo_park_api.entity.Usuario;
 import br.app.gestaotec.demo_park_api.repository.UsuarioRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +14,12 @@ public class UsuarioService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
     }
 }
