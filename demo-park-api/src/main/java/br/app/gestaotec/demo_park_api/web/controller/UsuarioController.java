0bb1e6381967d629897054af3015a3a29a8c9100
niltinfilho <niltonfilho115@gmail.com>
2025-10-30 13:43:00 -0300

    Documentando o recursos para alterar a senha

--- demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/web/controller/UsuarioController.java
@@ -56,6 +56,15 @@ public class UsuarioController {
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
+    @Operation(summary = "Atualizar senha", description = "Atualizar senha",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Senha atualizada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = void.class))),
+                    @ApiResponse(responseCode = "400", description = "Senha não confere",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Recurso nao encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto dto) {
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
