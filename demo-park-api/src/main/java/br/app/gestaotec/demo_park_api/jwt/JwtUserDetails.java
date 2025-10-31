8f4d8a0be5ff1490bc14b9fbf15610d4ce481fdf
niltinfilho <niltonfilho115@gmail.com>
2025-10-31 10:03:36 -0300

    A classe JwtUserDetails

---
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/jwt/JwtUserDetails.java
@@ -0,0 +1,24 @@
+package br.app.gestaotec.demo_park_api.jwt;
+
+import br.app.gestaotec.demo_park_api.entity.Usuario;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+
+public class JwtUserDetails extends User {
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return this.usuario.getId();
+    }
+
+    public String getRole() {
+        return this.usuario.getRole().name();
+    }
+}
