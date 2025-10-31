3161750871699c29ee26e64eaa5f2546d8dbb23d
niltinfilho <niltonfilho115@gmail.com>
2025-10-31 11:13:50 -0300

    Ajustes finais para o processo de autenticação e login

--- demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/service/UsuarioService.java
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/service/UsuarioService.java
@@ -6,6 +6,7 @@ import br.app.gestaotec.demo_park_api.exception.PasswordInvalidException;
 import br.app.gestaotec.demo_park_api.exception.UsernameUniqueViolationException;
 import br.app.gestaotec.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +16,12 @@ import java.util.List;
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
         try {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return usuarioRepository.save(usuario);
         } catch (org.springframework.dao.DataIntegrityViolationException ex) {
             throw new UsernameUniqueViolationException(String.format("Username '%s' já cadastrado", usuario.getUsername()));
@@ -39,11 +42,11 @@ public class UsuarioService {
         }
 
         Usuario user = buscarPorId(id);
-        if (!user.getPassword().equals(senhaAtual)) {
+        if (!passwordEncoder.matches(senhaAtual, user.getPassword())) {
             throw new PasswordInvalidException("Sua senha não confere.");
         }
 
-        user.setPassword(novaSenha);
+        user.setPassword(passwordEncoder.encode(novaSenha));
         return user;
     }
 
