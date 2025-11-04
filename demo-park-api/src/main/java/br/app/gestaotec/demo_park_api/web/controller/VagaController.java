d1239695fc9860f45df5aa3b8978a7c0a06fe36a
niltinfilho <niltonfilho115@gmail.com>
2025-11-04 13:48:52 -0300

    Adicionando os recursos Create e GetByCodigo em VagaController

---
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/web/controller/VagaController.java
@@ -0,0 +1,42 @@
+package br.app.gestaotec.demo_park_api.web.controller;
+
+import br.app.gestaotec.demo_park_api.entity.Vaga;
+import br.app.gestaotec.demo_park_api.service.VagaService;
+import br.app.gestaotec.demo_park_api.web.dto.VagaCreateDto;
+import br.app.gestaotec.demo_park_api.web.dto.VagaResponseDto;
+import br.app.gestaotec.demo_park_api.web.dto.mapper.VagaMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/vagas")
+public class VagaController {
+
+    private final VagaService vagaService;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDto dto) {
+        Vaga vaga = VagaMapper.toVaga(dto);
+        vagaService.salvar(vaga);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/{codigo}")
+                .buildAndExpand(vaga.getCodigo())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{codigo}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<VagaResponseDto> getByCodigo(@PathVariable String codigo) {
+        Vaga vaga = vagaService.buscarPorCodigo(codigo);
+        return ResponseEntity.ok(VagaMapper.toDto(vaga));
+    }
+}
