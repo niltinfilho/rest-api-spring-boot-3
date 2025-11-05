547fcfe06d37f1f51204c59542648d7009987575
niltinfilho <niltonfilho115@gmail.com>
2025-11-05 09:10:29 -0300

    Adicionando os DTOs e Mapper para o recurso de estacionamentos

---
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/web/dto/EstacionamentoCreateDto.java
@@ -0,0 +1,31 @@
+package br.app.gestaotec.demo_park_api.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstacionamentoCreateDto {
+    @NotBlank
+    @Size(min = 8, max = 8)
+    @Pattern(regexp = "[A-Z]{3}-[0-9]{4}", message = "A placa do veículo deve seguir o padrao 'XXX-0000'")
+    private String placa;
+    @NotBlank
+    private String marca;
+    @NotBlank
+    private String modelo;
+    @NotBlank
+    private String cor;
+    @NotBlank
+    @Size(min = 11, max = 11)
+    @CPF
+    private String clienteCpf;
+}
---
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/web/dto/mapper/ClienteVagaMapper.java
@@ -0,0 +1,19 @@
+package br.app.gestaotec.demo_park_api.web.dto.mapper;
+
+import br.app.gestaotec.demo_park_api.entity.ClienteVaga;
+import br.app.gestaotec.demo_park_api.web.dto.EstacionamentoCreateDto;
+import br.app.gestaotec.demo_park_api.web.dto.EstacionamentoResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClienteVagaMapper {
+    public static ClienteVaga toClienteVaga(EstacionamentoCreateDto dto) {
+        return new ModelMapper().map(dto, ClienteVaga.class);
+    }
+
+    public static EstacionamentoResponseDto toDto(ClienteVaga clienteVaga) {
+        return new ModelMapper().map(clienteVaga, EstacionamentoResponseDto.class);
+    }
+}
