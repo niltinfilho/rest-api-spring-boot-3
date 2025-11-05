f708d389968c6fbc84436ffdb2d977d78bde2528
niltinfilho <niltonfilho115@gmail.com>
2025-11-05 14:19:35 -0300

    Criando o recurso de check-out de veículo do estacionamento

--- demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/service/ClienteVagaService.java
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/service/ClienteVagaService.java
@@ -25,4 +25,9 @@ public class ClienteVagaService {
                 )
         );
     }
+
+    @Transactional(readOnly = true)
+    public long getTotalDeVezesEstacionamentoCompleto(String cpf) {
+        return repository.countByClienteCpfAndDataSaidaIsNotNull(cpf);
+    }
 }
