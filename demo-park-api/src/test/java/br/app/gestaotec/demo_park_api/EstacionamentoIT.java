ea98d21f983c680dac694569a7c0694c669d915a
niltinfilho <niltonfilho115@gmail.com>
2025-11-05 11:07:12 -0300

    Testando a operação de check-in com status 404

--- demo-park-api/src/test/java/br/app/gestaotec/demo_park_api/EstacionamentoIT.java
+++ demo-park-api/src/test/java/br/app/gestaotec/demo_park_api/EstacionamentoIT.java
@@ -61,6 +61,25 @@ public class EstacionamentoIT {
     }
 
     @Test
+    public void criarCheckIn_ComCpfInexistente_RetornarErrorStatus404() {
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+                .placa("WER-1111").marca("FIAT").modelo("PALIO")
+                .cor("AZUL").clienteCpf("20847314022")
+                .build();
+
+        testClient.post().uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo(404)
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
+    }
+
+    @Test
     public void criarCheckIn_ComDadosInvalidos_RetornarErrorStatus422() {
         EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
                 .placa("").marca("").modelo("")
@@ -75,6 +94,27 @@ public class EstacionamentoIT {
                 .expectStatus().isEqualTo(422)
                 .expectBody()
                 .jsonPath("status").isEqualTo(422)
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
+    }
+
+    @Sql(scripts = "/sql/estacionamentos/estacionamento-insert-vagas-ocupadas.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/sql/estacionamentos/estacionamento-delete-vagas-ocupadas.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Test
+    public void criarCheckIn_ComVagasOcupadas_RetornarErrorStatus404() {
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+                .placa("WER-1111").marca("FIAT").modelo("PALIO")
+                .cor("AZUL").clienteCpf("67431324030")
+                .build();
+
+        testClient.post().uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo("404")
                 .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
                 .jsonPath("method").isEqualTo("POST");
     }
