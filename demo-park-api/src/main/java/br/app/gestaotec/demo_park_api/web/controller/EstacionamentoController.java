67a467079e58104bc3acb8f5f4e0da23a2139139
niltinfilho <niltonfilho115@gmail.com>
2025-11-05 09:43:29 -0300

    A classe EstacionamentoController e o check-in de um veículo

---
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/web/controller/EstacionamentoController.java
@@ -0,0 +1,39 @@
+package br.app.gestaotec.demo_park_api.web.controller;
+
+import br.app.gestaotec.demo_park_api.entity.ClienteVaga;
+import br.app.gestaotec.demo_park_api.service.EstacionamentoService;
+import br.app.gestaotec.demo_park_api.web.dto.EstacionamentoCreateDto;
+import br.app.gestaotec.demo_park_api.web.dto.EstacionamentoResponseDto;
+import br.app.gestaotec.demo_park_api.web.dto.mapper.ClienteVagaMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/estacionamentos")
+public class EstacionamentoController {
+
+    private final EstacionamentoService estacionamentoService;
+
+    @PostMapping("/check-in")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<EstacionamentoResponseDto> checkIn(@RequestBody @Valid EstacionamentoCreateDto dto) {
+        ClienteVaga clienteVaga = ClienteVagaMapper.toClienteVaga(dto);
+        estacionamentoService.checkIn(clienteVaga);
+        EstacionamentoResponseDto responseDto = ClienteVagaMapper.toDto(clienteVaga);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/{recibo}")
+                .buildAndExpand(clienteVaga.getRecibo())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDto);
+    }
+}
