cdd090af3a4b21da152e4b3b213c6c90f693a79e
niltinfilho <niltonfilho115@gmail.com>
2025-11-05 14:11:06 -0300

    Desafios para calculo de custo e desconto

--- demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/util/EstacionamentoUtils.java
+++ demo-park-api/src/main/java/br/app/gestaotec/demo_park_api/util/EstacionamentoUtils.java
@@ -3,10 +3,19 @@ package br.app.gestaotec.demo_park_api.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EstacionamentoUtils {
+
+    private static final double PRIMEIROS_15_MINUTES = 5.00;
+    private static final double PRIMEIROS_60_MINUTES = 9.25;
+    private static final double ADICIONAL_15_MINUTES = 1.75;
+    private static final BigDecimal DESCONTO_PERCENTUAL = BigDecimal.valueOf(0.30);
+
 
     public static String gerarRecibo() {
         LocalDateTime date = LocalDateTime.now();
@@ -15,5 +24,33 @@ public class EstacionamentoUtils {
                 .replace("-", "")
                 .replace(":", "")
                 .replace("T", "-");
+    }
+
+    public static BigDecimal calcularCusto(LocalDateTime entrada, LocalDateTime saida) {
+        long minutes = entrada.until(saida, ChronoUnit.MINUTES);
+        double total = 0.0;
+        int adicional = 0;
+
+        if (minutes <= 15) {
+            total = PRIMEIROS_15_MINUTES;
+        } else if (minutes <= 60) {
+            total = PRIMEIROS_60_MINUTES;
+        } else {
+            minutes -= 60;
+            while (minutes > 0) {
+                adicional++;
+                minutes -= 15;
+            }
+            total = PRIMEIROS_60_MINUTES + (ADICIONAL_15_MINUTES * adicional);
+        }
+        return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+
+    public static BigDecimal calcularDesconto(BigDecimal custo, long numeroDeVezes) {
+        BigDecimal desconto = ((numeroDeVezes > 0) && (numeroDeVezes % 10 == 0))
+                ? custo.multiply(DESCONTO_PERCENTUAL)
+                : new BigDecimal(0);
+        return desconto.setScale(2, RoundingMode.HALF_EVEN);
     }
 }
